2e409ac312de05714d9078d3ab360d0a225d28b3
Andrew Oliveira <andrewoli60@gmail.com>
2023-06-13 23:34:31 -0300

    Get School

--- src/services/SchoolService.js
+++ src/services/SchoolService.js
@@ -7,3 +7,19 @@ export const CreateEscola = (data) => {
 
     return request;
 }
+
+export const getSchoolById = (id) => {
+    const request = axiosInstance.get(`${config.env.microservice.API_BASE_DRIVAN}/escola/${id}`)
+        .then((res) => Promise.resolve(res.data))
+        .catch((error) => Promise.reject(error))
+
+    return request;
+}
+
+export const getAllSchools = (id) => {
+    const request = axiosInstance.get(`${config.env.microservice.API_BASE_DRIVAN}/Escola/all/condutor//${id}`)
+        .then((res) => Promise.resolve(res.data))
+        .catch((error) => Promise.reject(error))
+
+    return request;
+}
