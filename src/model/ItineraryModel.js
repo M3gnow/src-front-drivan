09f4016610399fb7bab1b0ba5eaa551a8507eda3
Andrew Oliveira <andrewoli60@gmail.com>
2023-06-15 01:43:17 -0300

    ItineraryModel

--- src/model/ItineraryModel.js
+++ src/model/ItineraryModel.js
@@ -1,17 +1,33 @@
 export function builderAllItinerariesById(itinerariesResponseApi) {
-    const itineraries = itinerariesResponseApi.map((itinerario)=>{
+    const itineraries = itinerariesResponseApi.map((itinerario) => {
         return {
             id: itinerario.id,
             description: itinerario.descricao,
-            movements: itinerario.movimentacoes.map((movimentacao)=>{
+            movements: itinerario.movimentacoes.map((movimentacao) => {
                 return {
                     id: movimentacao.id,
                     school: { description: movimentacao.escola.descricao },
                     period: { description: movimentacao.periodo.descricao },
+                    hour: movimentacao.horario,
                     type: movimentacao.tipo === 1 ? "Entrada" : "Saída"
                 }
             })
         }
     })
     return itineraries;
+};
+
+export function builderItineraryFromService(user,data){
+    const itinerary = {
+        descricao: data.descriptionItinerario,
+        condutorId: user,
+        itinerarios: data.moviments.map((movement) => {
+            return {
+                escolaId: movement.school.id,
+                periodoId: movement.period.id,
+                tipoMovimentacao: movement.type
+            }
+        })
+    }
+    return itinerary;
 }
